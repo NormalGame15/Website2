ca54f78891467c92d27221592c369f59f33f784b
Derdt <deedointhilard@gmail.com>
2026-01-20 19:11:50 +0700

    Dedd

--- vue-project/postcss.config.js
+++ vue-project/postcss.config.js
@@ -1,6 +1,6 @@
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {}, // เปลี่ยนจาก 'tailwindcss' เป็น '@tailwindcss/postcss'
+    'autoprefixer': {},
   },
 }
